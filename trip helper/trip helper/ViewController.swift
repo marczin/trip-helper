930943185c06137ca95705b1fb4702387cf5b65c
Marcin Rosół <marcinrosol@MacBook-Pro-Marcin.local>
2021-01-14 13:52:56 +0100

    initial project

---
+++ trip helper/trip helper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trip helper
+//
+//  Created by Marcin Rosół on 14/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
